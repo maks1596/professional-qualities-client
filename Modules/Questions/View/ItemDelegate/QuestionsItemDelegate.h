39ad83ce78540ff6658a031e4b3cb73c87829b4b
Максим Шестопёров <maks1596@mail.ru>
2018-07-12 20:14:37 +0400

    Добавлен метод вычисления высоты в делегат списка

--- Modules/Questions/View/ItemDelegate/QuestionsItemDelegate.h
+++ Modules/Questions/View/ItemDelegate/QuestionsItemDelegate.h
@@ -6,6 +6,9 @@ class QuestionsItemDelegate : public QStyledItemDelegate {
 public:
     QuestionsItemDelegate(QObject *parent = nullptr);
 
+    QSize sizeHint(const QStyleOptionViewItem &option,
+                   const QModelIndex &index) const override;
+
     void paint(QPainter *painter,
                const QStyleOptionViewItem &option,
                const QModelIndex &index) const override;
